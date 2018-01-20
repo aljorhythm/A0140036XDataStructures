7b8898c8d2d2d8ed997396321bf2403080a1f092
Joel Lim <103879u@gmail.com>
2018-01-20 19:15:31 +0800

    Implement and Test queue

--- DataStructures/Queue.swift
+++ DataStructures/Queue.swift
@@ -7,47 +7,46 @@
  - Date: 2018
  */
 struct Queue<T> {
-
+	
+	var array: [T] = []
+	
     /// Adds an element to the tail of the queue.
     /// - Parameter item: The element to be added to the queue
     mutating func enqueue(_ item: T) {
+		array.append(item)
     }
 
     /// Removes an element from the head of the queue and return it.
     /// - Returns: item at the head of the queue
     mutating func dequeue() -> T? {
-        // TODO: Replace/remove the following line in your implementation.
-         return nil
+		return array.removeFirst()
     }
 
     /// Returns, but does not remove, the element at the head of the queue.
     /// - Returns: item at the head of the queue
     func peek() -> T? {
-        // TODO: Replace/remove the following line in your implementation.
-         return nil
+         return array.first
     }
 
     /// The number of elements currently in the queue.
     var count: Int {
-        // TODO: Replace/remove the following line in your implementation.
-         return 0
+         return array.count
     }
 
     /// Whether the queue is empty.
     var isEmpty: Bool {
-        // TODO: Replace/remove the following line in your implementation.
-        return false
+        return array.count == 0
     }
 
     /// Removes all elements in the queue.
     mutating func removeAll() {
+		array.removeAll()
     }
 
     /// Returns an array of the elements in their respective dequeue order, i.e.
     /// first element in the array is the first element to be dequeued.
     /// - Returns: array of elements in their respective dequeue order
     func toArray() -> [T] {
-        // TODO: Replace/remove the following line in your implementation.
-        return [T]()
+        return array
     }
 }
--- DataStructuresTests/QueueTests.swift
+++ DataStructuresTests/QueueTests.swift
@@ -13,15 +13,34 @@ class QueueTests: XCTestCase {
     }
 
     func testDequeue() {
-        // TODO: Fill in your test code here.
+		var queue = Queue<String>()
+		queue.enqueue("1")
+		queue.enqueue("2")
+		queue.enqueue("3")
+		
+		let next = queue.dequeue()
+		XCTAssertEqual(next, "1", "The item is not dequeued correctly!")
+		
+		XCTAssertEqual(queue.toArray(), ["2", "3"], "The item is not enqueued correctly!")
     }
 
     func testPeek() {
-        // TODO: Fill in your test code here.
+		var queue = Queue<String>()
+		queue.enqueue("1")
+		queue.enqueue("2")
+		queue.enqueue("3")
+		
+		let next = queue.peek()
+		XCTAssertEqual(next, "1", "The item is not peeked correctly!")
+		
+		XCTAssertEqual(queue.toArray(), ["1", "2", "3"], "The item should not be removed when peeked!")
     }
 
     func testCount() {
         var queue = Queue<String>()
+		
+		XCTAssertEqual(queue.count, 0, "The queue's length is not correct!");
+		
         queue.enqueue("1")
         queue.enqueue("2")
         queue.enqueue("3")
@@ -29,14 +48,28 @@ class QueueTests: XCTestCase {
     }
 
     func testIsEmpty() {
-        // TODO: Fill in your test code here.
+		var queue = Queue<String>()
+		
+		XCTAssertEqual(queue.isEmpty, true, "TThe queue should be empty")
+
+		queue.enqueue("a")
+		XCTAssertEqual(queue.isEmpty, false, "The queue should not be empty!")
     }
 
     func testRemoveAll() {
-        // TODO: Fill in your test code here.
+		var queue = Queue<String>()
+		queue.enqueue("1")
+		queue.enqueue("2")
+		queue.enqueue("3")
+		
+		queue.removeAll()
+		XCTAssertEqual(queue.isEmpty, true, "The queue should be empty after removing all elements!");
     }
 
     func testToArray() {
-        // TODO: Fill in your test code here.
+		var queue = Queue<String>()
+		let testArray = "abcdefgh".map { String ($0)}
+		testArray.forEach {queue.enqueue($0)}
+		XCTAssertEqual(queue.toArray(), testArray, "The queue is not converted to array correctly!");
     }
 }
