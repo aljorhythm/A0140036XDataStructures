b9581b0b5f55184e56415ceab192f3280b9d25d0
Joel Lim <103879u@gmail.com>
2018-01-22 00:46:15 +0800

    implement BreadthFirstOrderGenerator

--- DataStructures/BreadthFirstOrderGenerator.swift
+++ DataStructures/BreadthFirstOrderGenerator.swift
@@ -9,21 +9,53 @@
  */
 struct BreadthFirstOrderGenerator<Key: Hashable, Value: Collection>: IteratorProtocol,
     Sequence where Value.Iterator.Element == Key {
+	
+	private var arranged: [Key]
+	private var iteratorIndex = -1
 
+	/**
+		Breadth First Order algorithm
+	*/
+	static func bfs(_ graph: [Key: Value], subgraphRoot: Key, arranged: inout [Key], visitingQueue: inout Queue<Key>){
+		if !arranged.contains(subgraphRoot) {
+			arranged.append(subgraphRoot)
+		}
+		
+		if let children = graph[subgraphRoot] {
+			for childNode in children {
+				if !arranged.contains(childNode){
+					visitingQueue.enqueue(childNode)
+				}
+			}
+		}
+		
+		print(visitingQueue)
+		
+		while let nextRoot = visitingQueue.dequeue() {
+			bfs(graph, subgraphRoot: nextRoot, arranged: &arranged, visitingQueue: &visitingQueue)
+		}
+	}
+	
     /// Constructs a `BreadthFirstOrderGenerator` with the given graph and start
     /// node.
     /// - Parameters:
     ///   - graph: A dictionary of node to adjacency list pairs.
     ///   - start: The start node.
     init?(graph: [Key: Value], start: Key) {
+		arranged = []
+		var visitingQueue = Queue<Key>();
+		BreadthFirstOrderGenerator.bfs(graph, subgraphRoot: start, arranged: &arranged, visitingQueue: &visitingQueue)
     }
 
     func makeIterator() -> BreadthFirstOrderGenerator<Key, Value> {
         return self
     }
 
+	/**
+	- Returns: nil if end of sequence
+	*/
     mutating func next() -> Key? {
-        // TODO: Replace/remove the following line in your implementation.
-        return nil
+		iteratorIndex += 1
+		return arranged.count > iteratorIndex ? arranged[iteratorIndex] : nil
     }
 }
