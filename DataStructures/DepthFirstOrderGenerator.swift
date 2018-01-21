74a6c045f5cfe7214ebc82b43551348c180e7eac
Joel Lim <103879u@gmail.com>
2018-01-21 23:21:36 +0800

    passed testCyclicGraph

--- DataStructures/DepthFirstOrderGenerator.swift
+++ DataStructures/DepthFirstOrderGenerator.swift
@@ -9,21 +9,39 @@
  */
 struct DepthFirstOrderGenerator<Key: Hashable, Value: Collection>: IteratorProtocol,
     Sequence where Value.Iterator.Element == Key {
-
+	
+	 var arranged: [Key]
+	private var iteratorIndex = -1
+	
+	/**
+		Depth first search algorithm
+	*/
+	static func dfs(_ graph: [Key: Value], subgraphRoot: Key, arranged: inout [Key]){
+		if !arranged.contains(subgraphRoot) {
+			arranged.append(subgraphRoot)
+		}
+		if let children = graph[subgraphRoot] {
+			children.forEach { dfs( graph, subgraphRoot: $0, arranged: &arranged) }
+			return;
+		}
+	}
+	
     /// Constructs a `DepthFirstOrderGenerator` with the given graph and start
     /// node.
     /// - Parameters:
     ///   - graph: A dictionary of node to adjacency list pairs.
     ///   - start: The start node.
-    init?(graph: [Key: Value], start: Key) {
-    }
-
-    func makeIterator() -> DepthFirstOrderGenerator<Key, Value> {
-        return self
-    }
-
-    mutating func next() -> Key? {
-        // TODO: Replace/remove the following line in your implementation.
-        return nil
-    }
+	init?(graph: [Key: Value], start: Key) {
+		arranged = []
+		DepthFirstOrderGenerator.dfs(graph, subgraphRoot: start, arranged: &arranged)
+	}
+	
+	func makeIterator() -> DepthFirstOrderGenerator<Key, Value> {
+		return self
+	}
+	
+	mutating func next() -> Key? {
+		iteratorIndex += 1
+		return iteratorIndex < arranged.count ? arranged[iteratorIndex] : nil
+	}
 }
