6820348f2b6cd4bb2e5bde62c9a7302dc97d2c2b
Joel Lim <103879u@gmail.com>
2018-01-29 08:20:03 +0800

    implement BFS shortest path

--- DataStructures/BreadthFirstOrderGenerator.swift
+++ DataStructures/BreadthFirstOrderGenerator.swift
@@ -11,20 +11,31 @@
 public struct BreadthFirstOrderGenerator<Key: Hashable, Value: Collection>: IteratorProtocol,
     Sequence where Value.Iterator.Element == Key {
 
+	public typealias Graph = [Key: Value]
+	public typealias Path = [Key]
+
+	/**
+		Represents a node and the path it takes to get to it
+	*/
+	struct PathTo {
+		let key: Key
+		let path: Path
+	}
+
 	private var arranged: [Key]
 	private var iteratorIndex = -1
 
 	/**
 		Breadth First Order algorithm
 	*/
-	public static func bfs(_ graph: [Key: Value], subgraphRoot: Key) -> [Key] {
+	public static func bfs(_ graph: Graph, subgraphRoot: Key) -> [Key] {
 		var visitingQueue = Queue<Key>()
 		var arranged = [Key]()
 		bfs(graph, subgraphRoot: subgraphRoot, arranged: &arranged, visitingQueue: &visitingQueue)
 		return arranged
 	}
 
-	private static func bfs(_ graph: [Key: Value], subgraphRoot: Key, arranged: inout [Key], visitingQueue: inout Queue<Key>) {
+	private static func bfs(_ graph: Graph, subgraphRoot: Key, arranged: inout [Key], visitingQueue: inout Queue<Key>) {
 		if !arranged.contains(subgraphRoot) {
 			arranged.append(subgraphRoot)
 		}
@@ -42,14 +53,83 @@ public struct BreadthFirstOrderGenerator<Key: Hashable, Value: Collection>: Iter
 		}
 	}
 
+	/**
+		Shortest path using bfs
+	*/
+	private struct BfsShortestPath {
+
+		private var arranged = [Key]()
+		private var visitingQueue = Queue<PathTo>()
+		private(set) var shortestPath: [Key]?
+		private let end: Key
+		private let graph: Graph
+
+		/**
+			Finds a shortest path from one node to another in a graph
+		*/
+		init(graph: Graph, start: Key, end: Key) {
+			self.end = end
+			self.graph = graph
+			bfsShortestPath(start: start)
+		}
+
+		/**
+			Algorithm
+		**/
+		private mutating func bfsShortestPath(start: Key) {
+			bfsShortestPath(pathTo: PathTo(key: start, path: [start]))
+		}
+
+		private mutating func bfsShortestPath(pathTo: PathTo) {
+			let hasVisited = arranged.contains(pathTo.key)
+
+			var currentShortestPathLength: Int
+			if let length = shortestPath?.count {
+				currentShortestPathLength = length
+			} else {
+				currentShortestPathLength = Int.max
+			}
+
+			if end == pathTo.key && pathTo.path.count < currentShortestPathLength {
+				shortestPath = pathTo.path.count < currentShortestPathLength ? pathTo.path : shortestPath
+				return
+			}
+
+			if !hasVisited {
+				arranged.append(pathTo.key)
+			}
+
+			graph[pathTo.key]?.forEach {
+				if !arranged.contains($0) {
+					visitingQueue.enqueue(PathTo(key: $0, path: pathTo.path + [$0]))
+				}
+			}
+
+			while let nextRoot = visitingQueue.dequeue() {
+				bfsShortestPath(pathTo: nextRoot)
+			}
+		}
+	}
+
     /// Constructs a `BreadthFirstOrderGenerator` with the given graph and start
     /// node.
     /// - Parameters:
     ///   - graph: A dictionary of node to adjacency list pairs.
     ///   - start: The start node.
-    init?(graph: [Key: Value], start: Key) {
+    init?(graph: Graph, start: Key) {
 		arranged = BreadthFirstOrderGenerator.bfs(graph, subgraphRoot: start)
     }
+
+	/// Constructs a `BreadthFirstOrderGenerator` with the given graph, start
+	/// node and end node.
+	/// - Parameters:
+	///   - graph: A dictionary of node to adjacency list pairs.
+	///   - start: The start node.
+	///	  - end: The end node
+	init?(graph: Graph, start: Key, end: Key) {
+		let bfs = BfsShortestPath(graph: graph, start: start, end: end)
+		arranged = bfs.shortestPath ?? []
+	}
 
     public func makeIterator() -> BreadthFirstOrderGenerator<Key, Value> {
         return self
--- DataStructuresTests/BreadthFirstOrderGeneratorTests.swift
+++ DataStructuresTests/BreadthFirstOrderGeneratorTests.swift
@@ -5,6 +5,31 @@ import XCTest
 
 class BreadthFirstOrderGeneratorTests: XCTestCase {
 
+	func testShortestPath() {
+		let graphFileName = "graph_simple_bfs_shortest"
+		guard let graph = getGraph(graphFileName) else {
+			XCTFail("Path not found")
+			return
+		}
+		guard let shortestPath = BreadthFirstOrderGenerator(graph: graph, start: "A", end: "D") else {
+			XCTFail("Error finding shortest path")
+			return
+		}
+		XCTAssertEqual(Array(shortestPath), ["A", "C", "D"], "The  shortest path for \(graphFileName) is wrong")
+
+		guard let shortestPath2 = BreadthFirstOrderGenerator(graph: graph, start: "A", end: "B") else {
+			XCTFail("Error finding shortest path")
+			return
+		}
+		XCTAssertEqual(Array(shortestPath2), ["A", "C", "B"], "The  shortest path for \(graphFileName) is wrong")
+
+		guard let shortestPath3 = BreadthFirstOrderGenerator(graph: graph, start: "A", end: "E") else {
+			XCTFail("Error finding shortest path")
+			return
+		}
+		XCTAssertEqual(Array(shortestPath3), [], "The  shortest path for \(graphFileName) is wrong")
+	}
+
     func testCyclicGraph() {
         let graphFileName = "graph_cycle"
         XCTAssertEqual(getResultantNodesForGraph(graphFileName, startNode: "A"),
@@ -26,16 +51,25 @@ class BreadthFirstOrderGeneratorTests: XCTestCase {
             "The BFS for \(graphFileName) is wrong!")
     }
 
+	private func getGraph(_ fileName: String) -> [String: [String]]? {
+		// You do not need to modify this function.
+		guard let path = Bundle.main.path(forResource: fileName, ofType: "plist") else {
+			XCTFail("Path should not be nil.")
+			return nil
+		}
+		guard let dictionary = NSDictionary(contentsOfFile: path), let graph = dictionary as? [String: [String]] else {
+			XCTFail("Graph should not be nil.")
+			return nil
+		}
+		return graph
+	}
+
     private func getResultantNodesForGraph(_ fileName: String, startNode: String) -> [String] {
-        // You do not need to modify this function.
-        guard let path = Bundle.main.path(forResource: fileName, ofType: "plist") else {
-            XCTFail("Path should not be nil.")
-            return []
-        }
-        guard let dictionary = NSDictionary(contentsOfFile: path), let graph = dictionary as? [String: [String]] else {
-            XCTFail("Graph should not be nil.")
-            return []
-        }
+		guard let graph = getGraph(fileName) else {
+			XCTFail("Graph should not be nil.")
+			return []
+		}
+
         guard let bfsGenerator = BreadthFirstOrderGenerator(graph: graph, start: startNode) else {
             return []
         }
